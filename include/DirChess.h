0e8e7185d9a83a1799fb77a2f3b772d9278de09a
zwh-maker-z <zhang_wh@163.com>
2021-03-28 11:26:33 +0800

    基础搭好了

--- include/DirChess.h
+++ include/DirChess.h
@@ -1,10 +1,41 @@
 #ifndef __DIRCHESS_H
 #define __DIRCHESS_H
 
+#define DIRCHESS_SIDE_DI 1
+#define DIRCHESS_SIDE_DO 2
+
+#define DIRCHESS_ROTATE_RIGHT 1
+#define DIRCHESS_ROTATE_LEFT 3
+
+#define DIRCHESS_TYPE_LEFT_ARROW 1
+#define DIRCHESS_TYPE_UP_ARROW 2
+#define DIRCHESS_TYPE_RIGHT_ARROW 3
+#define DIRCHESS_TYPE_DOWN_ARROW 4
+#define DIRCHESS_TYPE_DOUBLE_ARROW 5
+#define DIRCHESS_TYPE_LEFT_BLOCK 6
+#define DIRCHESS_TYPE_UP_BLOCK 7
+#define DIRCHESS_TYPE_RIGHT_BLOCK 8
+#define DIRCHESS_TYPE_DOWN_BLOCK 9
+#define DIRCHESS_TYPE_BOSS 10
+#define DIRCHESS_TYPE_CENTER_BLOCK 11
+
+typedef struct Struct_DirStone{
+	int type;
+	int side;
+}DirStone;
+
 class DirChess{
 public:
 	DirChess();
+	void Init();//Init or reset
+	bool Move(int,int,int,int);//Move x0,y0 to x,y
+	bool Rotate(int,int,int);//Rotate x0,y0 by angle
+	bool Check();//Check if the player should rotate
+	int Side();//Return the side of the current player
+	int Win();//Return the winning side
 private:
+	DirStone chess[35];
+	int side=DIRCHESS_SIDE_DI;
 };
 
 #endif
--- src/DirChess.cpp
+++ src/DirChess.cpp
@@ -1,3 +1,4 @@
+#include <string.h>
 #include "DirChess.h"
 
 DirChess::DirChess()
@@ -5,3 +6,36 @@ DirChess::DirChess()
 
 }
 
+void DirChess::Init()
+{
+	memset(chess,0,sizeof(chess));
+	chess[7].side = DIRCHESS_SIDE_DO;
+	chess[7].type = DIRCHESS_TYPE_BOSS;
+	chess[27].side = DIRCHESS_SIDE_DI;
+	chess[27].type = DIRCHESS_TYPE_BOSS;
+}
+
+bool DirChess::Move(int x0,int y0,int x,int y)
+{
+	return false;	
+}
+
+bool DirChess::Rotate(int x0,int y0,int dir)
+{
+	return false;
+}
+
+bool DirChess::Check()
+{
+	return false;
+}
+
+int DirChess::Side()
+{
+	return DIRCHESS_SIDE_DO;	
+}
+
+int DirChess::Win()
+{
+	return DIRCHESS_SIDE_DI;
+}
